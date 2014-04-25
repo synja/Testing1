f75880cdb9f4e153e4213cbe829f5f91e9fb9c33
mrpaulb <mrpaulb@gmail.com>
2014-04-25 16:46:26 +0000

    testfactory added to test1.js

--- public/javascripts/test1.js
+++ public/javascripts/test1.js
@@ -6,6 +6,10 @@
 
 var myapp = angular.module('myapp',['ngRoute']);
 
+//=============================================
+//============       config        ============
+//=============================================
+
 
 myapp.config(function($routeProvider) {
    $routeProvider
@@ -20,10 +24,31 @@ myapp.config(function($routeProvider) {
    .otherwise({redirectTo:'/'});                  
 });
 
+//=============================================
+//============     factories       ============
+//=============================================
+
+
+//testfactory injected into Ctrl1 below
+myapp.factory('testfactory', function(){
+  var factory = {};
+  var customers = ['one','two','three'];
+  // var customers = [{name:'one'},{name:'two'},{name:'three'}];
+  factory.getCustomers = function (){ return customers };
+  return factory;
+}); 
+
+
+//=============================================
+//============    controllers      ============
+//=============================================
+
+
 var controllers = {}
 
-controllers.Ctrl1 = function($scope) {
-  $scope.friends = ['Rod','Jane','Freddy'];
+controllers.Ctrl1 = function($scope,testfactory) {
+  $scope.friends = testfactory.getCustomers();
+  // $scope.friends = ['Rod','Jane','Freddy'];
 };
 
 controllers.Ctrl2 = function($scope) {
