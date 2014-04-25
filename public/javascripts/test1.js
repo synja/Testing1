2523eefbbe9bda63ebafba4fc6cadadc2a7acf52
mrpaulb <mrpaulb@gmail.com>
2014-04-25 15:31:24 +0000

    module name myapp added, index2.html added with d3 cdn ref

--- public/javascripts/test1.js
+++ public/javascripts/test1.js
@@ -1,3 +1,18 @@
-function Ctrl($scope) {
+// myapp module
+// var name does not need to match module name
+// [] empty array of modules (dependencies)
+// myapp used with data-ng-app directive in html file
+
+var myapp = angular.module('myapp',[]);
+
+myapp.controller('Ctrl1',function($scope) {
         $scope.friends = ['Rod','Jane','Freddy'];
-}
+});
+
+myapp.controller('Ctrl2',function($scope) {
+        $scope.friends2 = [{name:'Rod', city:'Berlin'},
+                           {name:'Rob', city:'London'},
+                           {name:'Ron', city:'London'},
+                           {name:'Jane', city:'Paris'},
+                           {name:'Freddy', city:'Berlin'}];
+});
