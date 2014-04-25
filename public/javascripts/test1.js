68853f5440ef3fea5b25c11fb5e04c07e4838c66
mrpaulb <mrpaulb@gmail.com>
2014-04-25 16:22:02 +0000

    partials view1.html and view2.html added

--- public/javascripts/test1.js
+++ public/javascripts/test1.js
@@ -1,9 +1,24 @@
 // myapp module
 // var name does not need to match module name
 // [] empty array of modules (dependencies)
+// update 2014 04 25 16:56 ['ngRoute'] 
 // myapp used with data-ng-app directive in html file
 
-var myapp = angular.module('myapp',[]);
+var myapp = angular.module('myapp',['ngRoute']);
+
+
+myapp.config(function($routeProvider) {
+   $routeProvider
+   .when('/p1',{
+     controller:'Ctrl1',
+     templateUrl:'partials/view1.html'                   
+   })   
+   .when('/p2',{
+     controller:'Ctrl1',
+     templateUrl:'partials/view2.html'                   
+   })
+   .otherwise({redirectTo:'/'});                  
+});
 
 var controllers = {}
 
